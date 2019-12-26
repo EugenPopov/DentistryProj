ac73c3b1bf85ab54657f02cb48bb1e91d315745e
dzhezar <dzhezik@gmail.com>
2019-12-26 17:11:07 +0200

    Adaptive base is ready

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-require('../css/app.css');
-require('../css/media-queries/app_responsive.css');
-const jquery = require('jquery');
-import('bootstrap-material-design/dist/css/bootstrap-material-design.css');
-import('bootstrap-material-design/dist/js/bootstrap-material-design.js');
-
-
