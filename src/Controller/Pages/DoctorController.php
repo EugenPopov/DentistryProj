8e0641408ae275f7de6f0b097e1dd026c395f9c8
dzhezar <dzhezik@gmail.com>
2020-01-03 01:35:30 +0200

    doctors page started

--- src/Controller/Pages/DoctorController.php
+++ src/Controller/Pages/DoctorController.php
@@ -5,13 +5,38 @@ namespace App\Controller\Pages;
 
 
 use App\Entity\Doctor;
+use App\Service\CommonSettings\CommonSettingsInterface;
+use App\Service\DoctorService;
+use App\Service\MainPageSliderService;
+use App\Service\ReviewService;
+use App\Service\ServiceService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DoctorController extends AbstractController
 {
+    /**
+     * @var DoctorService
+     */
+    private $doctorService;
 
-    public function index(Doctor $doctor)
+    /**
+     * MainController constructor.
+     * @param DoctorService $doctorService
+     */
+    public function __construct(
+        DoctorService $doctorService
+    )
     {
-        dd($doctor->getSlug());
+        $this->doctorService = $doctorService;
+    }
+
+    public function index()
+    {
+        return $this->render('public/doctors/index.html.twig',[
+            'doctors' => $this->doctorService->findBy([], ['queue' => 'ASC'])]);
+    }
+
+    public function singleDoctor($slug)
+    {
     }
 }
