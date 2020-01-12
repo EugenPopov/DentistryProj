ccb62af748470129992891aad41686db38cf8f90
eugenpopov <zheny1995q@gmail.com>
2020-01-12 20:58:00 +0200

    started rendering Review page

--- src/Controller/Pages/ReviewController.php
+++ src/Controller/Pages/ReviewController.php
@@ -4,14 +4,31 @@
 namespace App\Controller\Pages;
 
 
+use App\Entity\Review;
+use App\Service\ReviewService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ReviewController extends AbstractController
 {
+    /**
+     * @var ReviewService
+     */
+    private $reviewService;
 
+    /**
+     * ReviewController constructor.
+     * @param ReviewService $reviewService
+     */
+    public function __construct(ReviewService $reviewService)
+    {
+        $this->reviewService = $reviewService;
+    }
 
     public function index()
     {
-        return $this->render('public/reviews.html.twig');
+        return $this->render('public/reviews.html.twig', [
+            'reviews' => $this->reviewService->findBy([],['queue' => 'ASC'])
+        ]);
     }
 }
