dde3f03ecd18c13620aa3291ee84815d1f0bcbe0
eugenpopov <zheny1995q@gmail.com>
2020-01-13 12:11:04 +0200

    Finished doctor page rendering

--- src/Controller/Pages/DoctorController.php
+++ src/Controller/Pages/DoctorController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Pages;
 use App\Entity\Doctor;
 use App\Service\DoctorService;
 use App\Service\SertificateService;
+use App\Service\WorksGalleryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DoctorController extends AbstractController
@@ -19,19 +20,26 @@ class DoctorController extends AbstractController
      * @var SertificateService
      */
     private $sertificateService;
+    /**
+     * @var WorksGalleryService
+     */
+    private $worksGalleryService;
 
     /**
      * MainController constructor.
      * @param DoctorService $doctorService
      * @param SertificateService $sertificateService
+     * @param WorksGalleryService $worksGalleryService
      */
     public function __construct(
         DoctorService $doctorService,
-        SertificateService $sertificateService
+        SertificateService $sertificateService,
+        WorksGalleryService $worksGalleryService
     )
     {
         $this->doctorService = $doctorService;
         $this->sertificateService = $sertificateService;
+        $this->worksGalleryService = $worksGalleryService;
     }
 
     public function index()
@@ -44,6 +52,9 @@ class DoctorController extends AbstractController
 
     public function singleDoctor(Doctor $doctor)
     {
-        return $this->render('public/doctors/single.html.twig', ['doctor' => $doctor]);
+        return $this->render('public/doctors/single.html.twig', [
+            'doctor' => $doctor,
+            'works' => $this->worksGalleryService->findBy([], ['queue' => 'ASC'])
+        ]);
     }
 }
