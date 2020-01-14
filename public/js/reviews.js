b59ded28fce9451dd3ef262d00befef67899caf3
dzhezar <dzhezik@gmail.com>
2020-01-14 12:40:03 +0200

    reviews page slider is done

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,9 +1,23 @@
 $('.reviews_slider').slick({
     dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    rows: 3,
+    slidesPerRow: 4,
     prevArrow: "<button class='prev'></button>",
     nextArrow: "<button class='next'></button>",
     appendArrows: $('.reviews_controls'),
+    responsive: [
+        {
+            breakpoint: 769,
+            settings: {
+                slidesPerRow: 3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesPerRow: 2,
+                rows:2
+            }
+        }
+    ]
 });
