abe36c27ed10698fe047b7c80ca3d151eb8b18e0
eugenpopov <zheny1995q@gmail.com>
2020-01-18 14:17:22 +0200

    Rendered works gallery

--- src/Controller/Pages/ReviewController.php
+++ src/Controller/Pages/ReviewController.php
@@ -7,6 +7,7 @@ namespace App\Controller\Pages;
 use App\Service\BlogService;
 use App\Service\PromotionService;
 use App\Service\ReviewService;
+use App\Service\WorksGalleryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ReviewController extends AbstractController
@@ -23,18 +24,24 @@ class ReviewController extends AbstractController
      * @var PromotionService
      */
     private $promotionService;
+    /**
+     * @var WorksGalleryService
+     */
+    private $galleryService;
 
     /**
      * ReviewController constructor.
      * @param ReviewService $reviewService
      * @param BlogService $blogService
      * @param PromotionService $promotionService
+     * @param WorksGalleryService $galleryService
      */
-    public function __construct(ReviewService $reviewService, BlogService $blogService, PromotionService $promotionService)
+    public function __construct(ReviewService $reviewService, BlogService $blogService, PromotionService $promotionService, WorksGalleryService $galleryService)
     {
         $this->reviewService = $reviewService;
         $this->blogService = $blogService;
         $this->promotionService = $promotionService;
+        $this->galleryService = $galleryService;
     }
 
     public function index()
@@ -42,7 +49,8 @@ class ReviewController extends AbstractController
         return $this->render('public/reviews.html.twig', [
             'reviews' => $this->reviewService->getInArray(),
             'articles' => $this->blogService->findBy([], ['queue' => 'ASC']),
-            'promotions' => $this->promotionService->findBy(['is_active' => true, 'is_public' => true], ['queue' => 'ASC'], 3)
+            'promotions' => $this->promotionService->findBy(['is_active' => true, 'is_public' => true], ['queue' => 'ASC'], 3),
+            'gallery' => $this->galleryService->findBy([], ['queue' => 'ASC'])
         ]);
     }
 }
