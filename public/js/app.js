68fe9a128f8d9778609d80402ff7419272adb1c6
eugenpopov <zheny1995q@gmail.com>
2020-01-22 13:18:43 +0200

    Added header locker

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,16 @@
 $('.page_up').on('click', function () {
     $('html,body').animate({scrollTop: $('.page_top_wrapper').offset().top}, 500, 'swing');
 });
+let running = false;
 $('#main-header-toggle').on('click', function () {
-    $('.mobile_nav-toogle').toggleClass('active');
-    $('#mobile_nav').slideToggle(500);
-    $('.main_header').toggleClass('active');
-
+    if(!running){
+        running = true;
+        $('.mobile_nav-toogle').toggleClass('active');
+        $('#mobile_nav').slideToggle(500, function () {
+            running = false;
+        });
+        $('.main_header').toggleClass('active');
+    }
 });
 function modalWindow(word) {
     $(".modal_text").text(word);
